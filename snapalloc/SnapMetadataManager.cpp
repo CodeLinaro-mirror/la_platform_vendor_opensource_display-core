7de03b6f5c9e1a64a83aacef8afb145d9a0f2d35
Dheepthi S <quic_dheepthi@quicinc.com>
2024-03-26 19:56:00 +0530

    snapalloc: Interlaced YUV PlaneLayout fix
    Recalculate and fix the plane layout offsets
    for Interlaced YUV formats
    
    Change-Id: I17311ed82da218fadf3d271b2ae55fc3426e9d29

--- snapalloc/SnapMetadataManager.cpp
+++ snapalloc/SnapMetadataManager.cpp
@@ -356,16 +356,12 @@ Error SnapMetadataManager::PlaneLayoutsHelper(SnapMetadata *metadata, SnapHandle
       desc.additionalOptions.emplace_back(modifier);
       BufferDescriptor out_desc;
       int out_priv_flags = 0;
-      auto err = constraint_mgr_->GetAllocationData(desc, &ad, &layout,
-                                                    &out_desc, &out_priv_flags);
-
+      auto err = constraint_mgr_->GetAllocationData(desc, &ad, &layout, &out_desc, &out_priv_flags);
       if (err != Error::NONE) {
         ALOGE("Invalid allocation - unable to create plane layout");
         return err;
       }
-
-      *static_cast<vendor_qti_hardware_display_common_BufferLayout *>(out_get) =
-          layout;
+      *static_cast<vendor_qti_hardware_display_common_BufferLayout *>(out_get) = layout;
     } else {
       *static_cast<vendor_qti_hardware_display_common_BufferLayout *>(out_get) =
           metadata->buffer_layout;
@@ -1235,6 +1231,9 @@ Error SnapMetadataManager::GetCustomDimensions(SnapHandleInternal *hnd, SnapMeta
                                .height = hnd->aligned_height,
                                .layerCount = static_cast<int32_t>(hnd->layer_count),
                                .reservedSize = static_cast<long>(hnd->reserved_size)};
+      static vendor_qti_hardware_display_common_KeyValuePair modifier = {
+          .key = "interlaced", .value = static_cast<uint64_t>(1)};
+      desc.additionalOptions.emplace_back(modifier);
       auto err = Error::NONE;
       err = constraint_mgr_->GetAllocationData(desc, &ad, &layout, &out_desc, &out_priv_flags);
       if (err != Error::NONE) {
@@ -1242,7 +1241,9 @@ Error SnapMetadataManager::GetCustomDimensions(SnapHandleInternal *hnd, SnapMeta
         return err;
       }
       *stride = static_cast<int>(layout.aligned_width_in_bytes / layout.bpp);
-      *height = static_cast<int>(layout.aligned_height);
+      // Get re-aligned height for single ubwc interlaced field and
+      // multiply by 2 to get frame height.
+      *height = static_cast<int>(layout.aligned_height * 2);
     }
   }
   return Error::NONE;
--- snapalloc/UBWCPolicy.cpp
+++ snapalloc/UBWCPolicy.cpp
@@ -289,11 +289,13 @@ Error UBWCPolicy::GetUBWCAlloc(BufferDescriptor desc, UBWCCapabilities caps, All
   // TODO: Remove hard-coding
   int alignment = 4096;
   int height = desc.height;
+  int interlaced = 0;
   // Divide input height by 2 for interlaced case
   for (auto &type : desc.additionalOptions) {
     if (std::strcmp(type.key, "interlaced") == 0) {
       if (type.value == 1) {
         height = (height + 1) >> 1;
+        interlaced = 1;
         ;
       }
     }
@@ -481,6 +483,24 @@ Error UBWCPolicy::GetUBWCAlloc(BufferDescriptor desc, UBWCCapabilities caps, All
       return Error::UNSUPPORTED;
     }
   }
+
+  if (interlaced) {
+    vendor_qti_hardware_display_common_BufferLayout temp_layout = *out_layout;
+    if (IsYuv(desc.format)) {
+      // In interlaced & compressed case plane count is 8 then plane_info[0], plane_info[1],
+      // plane_info[4] & plane_info[5] will contain info about Y_plane, UV_plane, Y_plane
+      // & UV_plane. Remaining plane will contain info about the meta planes. Calculate the
+      // offset values accordingly
+      out_layout->plane_count = temp_layout.plane_count * 2;
+      int field_base = temp_layout.planes[1].offset_in_bytes + temp_layout.planes[1].size_in_bytes;
+      for (int plane_count = 0; plane_count < temp_layout.plane_count; plane_count++) {
+        out_layout->planes[plane_count] = temp_layout.planes[plane_count];
+        out_layout->planes[plane_count + temp_layout.plane_count] = temp_layout.planes[plane_count];
+        out_layout->planes[plane_count + temp_layout.plane_count].offset_in_bytes += field_base;
+      }
+    }
+  }
+
   return Error::NONE;
 #endif
 
