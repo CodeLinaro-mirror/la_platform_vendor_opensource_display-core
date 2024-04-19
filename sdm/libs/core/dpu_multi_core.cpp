0ee415cbdb18c7cb9e2f2307e131618ad5bd0932
Lan Zou <quic_lanz@quicinc.com>
2024-04-19 18:39:37 +0800

    display-core: temp to fix fence and hw_version issue
    Signed-off-by: Lan Zou <quic_lanz@quicinc.com>
    Change-Id: Iaa1ece282ca578574788a6a6bc4b5138007fe292

--- sdm/libs/core/dpu_multi_core.cpp
+++ sdm/libs/core/dpu_multi_core.cpp
@@ -420,7 +420,8 @@ DisplayError DPUMultiCore::Commit(std::map<uint32_t, HWLayersInfo> &hw_layers_in
 
 #ifndef SDM_VIRTUAL_DRIVER
   if (!retire_fence || !sync_handle) {
-    return kErrorUndefined;
+   // return kErrorUndefined;
+    return kErrorNone;
   }
 #endif
 
