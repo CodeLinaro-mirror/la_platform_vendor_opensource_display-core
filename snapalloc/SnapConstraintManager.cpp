21a9ff40d3d49d0678de7cff2ef4f589896730df
Tharaga Balachandran <quic_tbalacha@quicinc.com>
2024-06-11 15:07:12 -0400

    snapalloc: Fix plane layouts for YCbCr_422_I
    Update plane layouts for YCbCr_422_I to make
    the format lockable in Android and add
    support in the graphics constraint provider.
    
    Change-Id: I4a6c99b71f14155fff21a0d10886a0ebdf3cb092
    (cherry picked from commit e204a230585abfaa72574f58059f21f2fc6f4863)
    (cherry picked from commit 2de32518d95a8cf1fd38c038589b32842acadb8d)
    (cherry picked from commit 5ce48057b7c0e8c6c900d82aa23d913ee713003f)

--- snapalloc/SnapConstraintManager.cpp
+++ snapalloc/SnapConstraintManager.cpp
@@ -348,14 +348,29 @@ Error SnapConstraintManager::ConstraintsToBufferLayout(
         ALIGN(layout->planes[i].horizontal_stride_in_bytes * layout->planes[i].scanlines,
               constraints->planes[i].size_align);
     layout->planes[i].offset_in_bytes = offset_sum;
+
+    if (desc.format == YCBCR_422_I) {
+      // For interleaved formats, the stride for all components is the same
+      // but the sizes must factor in subsampling
+      layout->planes[i].size_in_bytes =
+          ALIGN((layout->planes[i].horizontal_stride_in_bytes /
+                 layout->planes[i].horizontal_subsampling) *
+                    (layout->planes[i].scanlines / layout->planes[i].vertical_subsampling),
+                constraints->planes[i].size_align);
+
+      layout->planes[i].offset_in_bytes = layout->planes[i].components[0].offset_in_bits / 8.0;
+      layout->planes[i].components[0].offset_in_bits = 0;
+    }
+
     offset_sum += layout->planes[i].size_in_bytes;
     ALOGD_IF(DEBUG,
              "%s: format %d, layout->planes[i].horizontal_stride_in_bytes %d "
              "layout->planes[i].scanlines %d "
-             " constraints->planes[i].size_align %d layout->planes[i].size_in_bytes %d",
+             " constraints->planes[i].size_align %d layout->planes[i].size_in_bytes %d"
+             " layout->planes[%d].offset_in_bytes %d",
              __FUNCTION__, desc.format, layout->planes[i].horizontal_stride_in_bytes,
              layout->planes[i].scanlines, constraints->planes[i].size_align,
-             layout->planes[i].size_in_bytes);
+             layout->planes[i].size_in_bytes, i, layout->planes[i].offset_in_bytes);
     layout->size_in_bytes += layout->planes[i].size_in_bytes;
   }
   layout->size_in_bytes = ALIGN(layout->size_in_bytes, constraints->size_align_bytes);
