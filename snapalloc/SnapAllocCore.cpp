e4f8b56c754486173cfa40013d6e883b4f83936f
Bikki Srinivasa Rao <quic_bikksrin@quicinc.com>
2024-09-04 12:44:28 +0530

    Snapalloc: Added NULL check after malloc
    After the malloc operation, checking whether the pointer
    is null or not.
    
    Change-Id: Ie2f0e4809f8d07c03c4ce1249d84f46794450c56
    (cherry picked from commit 5d6b463799c8b7ee25484a5a3d3524db1a1e442b)

--- snapalloc/SnapAllocCore.cpp
+++ snapalloc/SnapAllocCore.cpp
@@ -87,6 +87,10 @@ Error SnapAllocCore::Allocate(BufferDescriptor desc, int count,
         out_desc.format, layout.aligned_width_in_bytes, &aligned_width_in_pixels);
 
     SnapHandleInternal *hnd = static_cast<SnapHandleInternal *>(malloc(sizeof(SnapHandleInternal)));
+    if(hnd == nullptr) {
+      ALOGE("Failed to allocate memory for hnd");
+      return Error::NO_RESOURCES;
+    }
     hnd->Init(ad.fd, m_data.fd, ad.flags, layout.aligned_width_in_bytes, aligned_width_in_pixels,
               layout.aligned_height, desc.width, desc.height, out_desc.format, buffer_type, ad.size,
               desc.usage);
