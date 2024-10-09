AetheriaOS/aetheria_vendor_qcom_opensource_display-core

snapalloc: Debug Log cleanup
- Changes to property approach instead of compile time
  constants to allow enabling debug logs without
  re-compiling
- Change ALOG to DLOG
- Suppress error logs in constraint providers with warning
  Throw an error only on failing to allocate for the given
  request
- Remove liblog shared lib to decouple android
  dependency

Change-Id: Iec4b23476f0ed9bfc3c587bf65b85cc58cb20cf2

snapalloc/DisplayConstraintProvider.cpp
@@ -1,17 +1,13 @@
-// Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights reserved.
+// Copyright (c) 2023-2024 Qualcomm Innovation Center, Inc. All rights reserved.
 // SPDX-License-Identifier: BSD-3-Clause-Clear
 
 #include "DisplayConstraintProvider.h"
 
 #include <dlfcn.h>
-#include <log/log.h>
-
 #include <fstream>
 #include <iostream>
 
 #include "SnapConstraintParser.h"
-
-#define DEBUG 0
 
 namespace snapalloc {
 DisplayConstraintProvider *DisplayConstraintProvider::instance_{nullptr};
@@ -45,10 +41,10 @@ int DisplayConstraintProvider::GetCapabilities(BufferDescriptor desc, Capability
   if (desc.usage & vendor_qti_hardware_display_common_BufferUsage::COMPOSER_OVERLAY ||
       desc.usage & vendor_qti_hardware_display_common_BufferUsage::COMPOSER_CLIENT_TARGET ||
       desc.usage & vendor_qti_hardware_display_common_BufferUsage::COMPOSER_CURSOR) {
-    ALOGD_IF(DEBUG, "DisplayConstraintProvider is enabled");
+    DLOGD_IF(enable_logs, "DisplayConstraintProvider is enabled");
     out->enabled = true;
   } else {
-    ALOGD_IF(DEBUG, "DisplayConstraintProvider is not enabled");
+    DLOGD_IF(enable_logs, "DisplayConstraintProvider is not enabled");
     out->enabled = false;
   }
 
@@ -61,7 +57,7 @@ int DisplayConstraintProvider::GetCapabilities(BufferDescriptor desc, Capability
 
 int DisplayConstraintProvider::BuildConstraints(BufferDescriptor desc, BufferConstraints *data) {
   if (format_data_map_.find(desc.format) == format_data_map_.end()) {
-    ALOGE("Could not find entry for format %lu", static_cast<uint64_t>(desc.format));
+    DLOGW("Could not find entry for format %lu", static_cast<uint64_t>(desc.format));
     return -1;
   }
 
@@ -85,7 +81,7 @@ int DisplayConstraintProvider::BuildConstraints(BufferDescriptor desc, BufferCon
               pixel_format_modifier),
           false);  // false indicates not ubwc
       if (mmm_color_format < 0) {
-        ALOGE("Failed to get format mapping to use mmm_color_fmt");
+        DLOGW("Failed to get format mapping to use mmm_color_fmt");
         return -1;
       }
       switch (component_type) {
@@ -120,12 +116,12 @@ int DisplayConstraintProvider::BuildConstraints(BufferDescriptor desc, BufferCon
 int DisplayConstraintProvider::GetConstraints(BufferDescriptor desc, BufferConstraints *out) {
   (void)out;
 #ifdef __ANDROID__
-  ALOGD_IF(DEBUG, "Using display libs for alignment calculations");
+  DLOGD_IF(enable_logs, "Using display libs for alignment calculations");
   BufferConstraints data;
   int status = 0;
   status = BuildConstraints(desc, &data);
   if (status != Error::NONE) {
-    ALOGW("Error while getting constraints from display libs width %d, height %d, format %d",
+    DLOGW("Error while getting constraints from display libs width %d, height %d, format %d",
           desc.width, desc.height, static_cast<uint64_t>(desc.format));
     return -1;
   }
@@ -133,13 +129,13 @@ int DisplayConstraintProvider::GetConstraints(BufferDescriptor desc, BufferConst
   return 0;
 #endif
   if (constraint_set_map_.empty()) {
-    ALOGE("DisplayConstraintProvider constraint set map is empty");
+    DLOGW("DisplayConstraintProvider constraint set map is empty");
     return -1;
   }
   if (constraint_set_map_.find(desc.format) != constraint_set_map_.end()) {
     *out = constraint_set_map_.at(desc.format);
   } else {
-    ALOGE("DisplayConstraintProvider could not find entry for format %lu",
+    DLOGW("DisplayConstraintProvider could not find entry for format %lu",
           static_cast<uint64_t>(desc.format));
   }
   return 0;

snapalloc/SnapConstraintParser.cpp
@@ -1,10 +1,9 @@
-// Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights reserved.
+// Copyright (c) 2023-2024 Qualcomm Innovation Center, Inc. All rights reserved.
 // SPDX-License-Identifier: BSD-3-Clause-Clear
 
 #include "SnapConstraintParser.h"
 
 #include <json/json.h>
-#include <log/log.h>
 #include <fstream>
 #include <iostream>
 
@@ -134,7 +133,7 @@ bool SnapConstraintParser::StringToEnumType(
     return true;
   }
 
-  ALOGE("Unable to find enum value for format string %s", input.c_str());
+  DLOGW("Unable to find enum value for format string %s", input.c_str());
   return false;
 }
 
@@ -148,7 +147,7 @@ bool SnapConstraintParser::StringToEnumType(
 
   // Empty string valid for formats where plane layout is not queried (e.g., depth stencil formats)
   if (input != "") {
-    ALOGE("Unable to find enum value for plane layout component string %s", input.c_str());
+    DLOGW("Unable to find enum value for plane layout component string %s", input.c_str());
   }
 
   return false;
@@ -160,7 +159,7 @@ int SnapConstraintParser::ParseFormats(
 
   std::ifstream ifs(json_path.c_str());
   if (!ifs.is_open()) {
-    ALOGE("Error opening file");
+    DLOGE("Error opening file");
     return -1;
   }
 
@@ -173,7 +172,7 @@ int SnapConstraintParser::ParseFormats(
 
     vendor_qti_hardware_display_common_PixelFormat format;
     if (!StringToEnumType(format_data_set["format"].asString(), &format)) {
-      ALOGE("%s: Could not find format %s in format list", __FUNCTION__,
+      DLOGW("%s: Could not find format %s in format list", __FUNCTION__,
             format_data_set["format"].asString().c_str());
       continue;
     }
@@ -209,8 +208,8 @@ int SnapConstraintParser::ParseFormats(
         } else {
           // Empty string valid for formats where plane layout is not queried (e.g., depth stencil formats)
           if ((plane_component["component_type"].asString() != "")) {
-            ALOGE("Invalid component type %s in %s",
-                plane_component["component_type"].asString().c_str(), json_path.c_str());
+            DLOGW("Invalid component type %s in %s",
+                  plane_component["component_type"].asString().c_str(), json_path.c_str());
             continue;
           }
         }
@@ -222,7 +221,7 @@ int SnapConstraintParser::ParseFormats(
   }
 
   if (format_data_map->empty()) {
-    ALOGE("Format map empty");
+    DLOGE("Format map empty");
     return -1;
   }
 
@@ -235,7 +234,7 @@ int SnapConstraintParser::ParseAlignments(const std::string &json_path,
   std::ifstream ifs(json_path.c_str());
 
   if (!ifs.is_open()) {
-    ALOGE("Error opening file");
+    DLOGE("Error opening file");
     return -1;
   }
 
@@ -252,7 +251,7 @@ int SnapConstraintParser::ParseAlignments(const std::string &json_path,
     vendor_qti_hardware_display_common_PixelFormat format =
         vendor_qti_hardware_display_common_PixelFormat::PIXEL_FORMAT_UNSPECIFIED;
     if (!StringToEnumType(constraint_set["format"].asString(), &format)) {
-      ALOGE("%s: Could not find format %s in format list", __FUNCTION__,
+      DLOGW("%s: Could not find format %s in format list", __FUNCTION__,
             constraint_set["format"].asString().c_str());
       continue;
     }
@@ -298,7 +297,7 @@ int SnapConstraintParser::ParseAlignments(const std::string &json_path,
         if (StringToEnumType(plane_component["component_type"].asString(), &component_type)) {
           plane_constraints.components.push_back(component_type);
         } else {
-          ALOGE("Invalid component type %s in %s",
+          DLOGW("Invalid component type %s in %s",
                 plane_component["component_type"].asString().c_str(), json_path.c_str());
           continue;
         }
@@ -310,7 +309,7 @@ int SnapConstraintParser::ParseAlignments(const std::string &json_path,
   }
 
   if (constraint_set_map->empty()) {
-    ALOGE("Format map empty");
+    DLOGE("Format map empty");
   }
   return 0;
 }

CodeLinaro-mirror/la_platform_vendor_opensource_display-core

snapalloc/DisplayConstraintProvider.cpp
@@ -4,14 +4,10 @@
 #include "DisplayConstraintProvider.h"
 
 #include <dlfcn.h>
-#include <log/log.h>
-
 #include <fstream>
 #include <iostream>
 
 #include "SnapConstraintParser.h"
-
-#define DEBUG 0
 
 namespace snapalloc {
 DisplayConstraintProvider *DisplayConstraintProvider::instance_{nullptr};
@@ -45,10 +41,10 @@ int DisplayConstraintProvider::GetCapabilities(BufferDescriptor desc, Capability
   if (desc.usage & vendor_qti_hardware_display_common_BufferUsage::COMPOSER_OVERLAY ||
       desc.usage & vendor_qti_hardware_display_common_BufferUsage::COMPOSER_CLIENT_TARGET ||
       desc.usage & vendor_qti_hardware_display_common_BufferUsage::COMPOSER_CURSOR) {
-    ALOGD_IF(DEBUG, "DisplayConstraintProvider is enabled");
+    DLOGD_IF(enable_logs, "DisplayConstraintProvider is enabled");
     out->enabled = true;
   } else {
-    ALOGD_IF(DEBUG, "DisplayConstraintProvider is not enabled");
+    DLOGD_IF(enable_logs, "DisplayConstraintProvider is not enabled");
     out->enabled = false;
   }
 
@@ -61,7 +57,7 @@ int DisplayConstraintProvider::GetCapabilities(BufferDescriptor desc, Capability
 
 int DisplayConstraintProvider::BuildConstraints(BufferDescriptor desc, BufferConstraints *data) {
   if (format_data_map_.find(desc.format) == format_data_map_.end()) {
-    ALOGE("Could not find entry for format %lu", static_cast<uint64_t>(desc.format));
+    DLOGE("Could not find entry for format %lu", static_cast<uint64_t>(desc.format));
     return -1;
   }
 
@@ -85,7 +81,7 @@ int DisplayConstraintProvider::BuildConstraints(BufferDescriptor desc, BufferCon
               pixel_format_modifier),
           false);  // false indicates not ubwc
       if (mmm_color_format < 0) {
-        ALOGE("Failed to get format mapping to use mmm_color_fmt");
+        DLOGE("Failed to get format mapping to use mmm_color_fmt");
         return -1;
       }
       switch (component_type) {
@@ -121,12 +117,12 @@ int DisplayConstraintProvider::BuildConstraints(BufferDescriptor desc, BufferCon
 int DisplayConstraintProvider::GetConstraints(BufferDescriptor desc, BufferConstraints *out) {
   (void)out;
 #ifdef __ANDROID__
-  ALOGD_IF(DEBUG, "Using display libs for alignment calculations");
+  DLOGD_IF(enable_logs, "Using display libs for alignment calculations");
   BufferConstraints data;
   int status = 0;
   status = BuildConstraints(desc, &data);
   if (status != Error::NONE) {
-    ALOGW("Error while getting constraints from display libs width %d, height %d, format %d",
+    DLOGW("Error while getting constraints from display libs width %d, height %d, format %d",
           desc.width, desc.height, static_cast<uint64_t>(desc.format));
     return -1;
   }
@@ -134,13 +130,13 @@ int DisplayConstraintProvider::GetConstraints(BufferDescriptor desc, BufferConst
   return 0;
 #endif
   if (constraint_set_map_.empty()) {
-    ALOGE("DisplayConstraintProvider constraint set map is empty");
+    DLOGE("DisplayConstraintProvider constraint set map is empty");
     return -1;
   }
   if (constraint_set_map_.find(desc.format) != constraint_set_map_.end()) {
     *out = constraint_set_map_.at(desc.format);
   } else {
-    ALOGE("DisplayConstraintProvider could not find entry for format %lu",
+    DLOGE("DisplayConstraintProvider could not find entry for format %lu",
           static_cast<uint64_t>(desc.format));
   }
   return 0;

snapalloc/SnapConstraintParser.cpp
@@ -1,10 +1,9 @@
-// Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights reserved.
+// Copyright (c) 2023-2024 Qualcomm Innovation Center, Inc. All rights reserved.
 // SPDX-License-Identifier: BSD-3-Clause-Clear
 
 #include "SnapConstraintParser.h"
 
 #include <json/json.h>
-#include <log/log.h>
 #include <fstream>
 #include <iostream>
 
@@ -134,7 +133,7 @@ bool SnapConstraintParser::StringToEnumType(
     return true;
   }
 
-  ALOGE("Unable to find enum value for format string %s", input.c_str());
+  DLOGE("Unable to find enum value for format string %s", input.c_str());
   return false;
 }
 
@@ -148,7 +147,7 @@ bool SnapConstraintParser::StringToEnumType(
 
   // Empty string valid for formats where plane layout is not queried (e.g., depth stencil formats)
   if (input != "") {
-    ALOGE("Unable to find enum value for plane layout component string %s", input.c_str());
+    DLOGE("Unable to find enum value for plane layout component string %s", input.c_str());
   }
 
   return false;
@@ -160,7 +159,7 @@ int SnapConstraintParser::ParseFormats(
 
   std::ifstream ifs(json_path.c_str());
   if (!ifs.is_open()) {
-    ALOGE("Error opening file");
+    DLOGE("Error opening file");
     return -1;
   }
 
@@ -173,7 +172,7 @@ int SnapConstraintParser::ParseFormats(
 
     vendor_qti_hardware_display_common_PixelFormat format;
     if (!StringToEnumType(format_data_set["format"].asString(), &format)) {
-      ALOGE("%s: Could not find format %s in format list", __FUNCTION__,
+      DLOGE("%s: Could not find format %s in format list", __FUNCTION__,
             format_data_set["format"].asString().c_str());
       continue;
     }
@@ -209,7 +208,7 @@ int SnapConstraintParser::ParseFormats(
         } else {
           // Empty string valid for formats where plane layout is not queried (e.g., depth stencil formats)
           if ((plane_component["component_type"].asString() != "")) {
-            ALOGE("Invalid component type %s in %s",
+            DLOGE("Invalid component type %s in %s",
                 plane_component["component_type"].asString().c_str(), json_path.c_str());
             continue;
           }
@@ -222,7 +221,7 @@ int SnapConstraintParser::ParseFormats(
   }
 
   if (format_data_map->empty()) {
-    ALOGE("Format map empty");
+    DLOGE("Format map empty");
     return -1;
   }
 
@@ -235,7 +234,7 @@ int SnapConstraintParser::ParseAlignments(const std::string &json_path,
   std::ifstream ifs(json_path.c_str());
 
   if (!ifs.is_open()) {
-    ALOGE("Error opening file");
+    DLOGE("Error opening file");
     return -1;
   }
 
@@ -252,7 +251,7 @@ int SnapConstraintParser::ParseAlignments(const std::string &json_path,
     vendor_qti_hardware_display_common_PixelFormat format =
         vendor_qti_hardware_display_common_PixelFormat::PIXEL_FORMAT_UNSPECIFIED;
     if (!StringToEnumType(constraint_set["format"].asString(), &format)) {
-      ALOGE("%s: Could not find format %s in format list", __FUNCTION__,
+      DLOGE("%s: Could not find format %s in format list", __FUNCTION__,
             constraint_set["format"].asString().c_str());
       continue;
     }
@@ -298,7 +297,7 @@ int SnapConstraintParser::ParseAlignments(const std::string &json_path,
         if (StringToEnumType(plane_component["component_type"].asString(), &component_type)) {
           plane_constraints.components.push_back(component_type);
         } else {
-          ALOGE("Invalid component type %s in %s",
+          DLOGE("Invalid component type %s in %s",
                 plane_component["component_type"].asString().c_str(), json_path.c_str());
           continue;
         }
@@ -310,7 +309,7 @@ int SnapConstraintParser::ParseAlignments(const std::string &json_path,
   }
 
   if (constraint_set_map->empty()) {
-    ALOGE("Format map empty");
+    DLOGE("Format map empty");
   }
   return 0;
 }