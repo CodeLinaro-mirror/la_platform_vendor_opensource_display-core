AetheriaOS/aetheria_vendor_qcom_opensource_display-core

snapalloc: Fail allocation if GPU bits are incorrectly set
Fail allocation for a subset of formats that are not
sampleable or renderable by GPU. These formats should not be
allocated with GPU texture or render usage bits set.

Change-Id: I6b7cd16d2497dd61908f5ae49c2466dc6456982f

snapalloc/SnapConstraintManager.cpp
@@ -161,6 +161,16 @@ Error SnapConstraintManager::GetAllocationData(
       return Error::BAD_VALUE;
     }
   }
+
+  if ((
+      in_desc.format == vendor_qti_hardware_display_common_PixelFormat::RAW10 ||
+      in_desc.format == vendor_qti_hardware_display_common_PixelFormat::RAW12 ||
+      in_desc.format == vendor_qti_hardware_display_common_PixelFormat::YCBCR_422_SP)
+      && (in_desc.usage & GPU_RENDER_TARGET || in_desc.usage & GPU_TEXTURE)) {
+        DLOGE("Failing allocation for unsupported formats for GPU render/texture");
+        return Error::BAD_VALUE;
+  }
+
   if (in_desc.format == vendor_qti_hardware_display_common_PixelFormat::IMPLEMENTATION_DEFINED ||
       in_desc.format == vendor_qti_hardware_display_common_PixelFormat::YCBCR_420_888) {
     vendor_qti_hardware_display_common_PixelFormatModifier modifier = PIXEL_FORMAT_MODIFIER_NONE;

CodeLinaro-mirror/la_platform_vendor_opensource_display-core

allocated with GPU texture or render usage bits set
(cherry picked from commit 7c552293bb5c46893bf80a76f33d6893c1329ff2)
(cherry picked from commit 95a936f2247b821d65d04b36b6cb15914babecec)

snapalloc/SnapConstraintManager.cpp
@@ -157,6 +157,16 @@ Error SnapConstraintManager::GetAllocationData(
       return Error::BAD_VALUE;
     }
   }
+
+  if ((
+      in_desc.format == vendor_qti_hardware_display_common_PixelFormat::RAW10 ||
+      in_desc.format == vendor_qti_hardware_display_common_PixelFormat::RAW12 ||
+      in_desc.format == vendor_qti_hardware_display_common_PixelFormat::YCBCR_422_SP)
+      && (in_desc.usage & GPU_RENDER_TARGET || in_desc.usage & GPU_TEXTURE)) {
+        ALOGE("Failing allocation for unsupported formats for GPU render/texture");
+        return Error::BAD_VALUE;
+  }
+
   if (in_desc.format == vendor_qti_hardware_display_common_PixelFormat::IMPLEMENTATION_DEFINED ||
       in_desc.format == vendor_qti_hardware_display_common_PixelFormat::YCBCR_420_888) {
     vendor_qti_hardware_display_common_PixelFormatModifier modifier = PIXEL_FORMAT_MODIFIER_NONE;