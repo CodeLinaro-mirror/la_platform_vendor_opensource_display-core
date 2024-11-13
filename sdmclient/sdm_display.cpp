2e2cbaa2cae19ef0df978867747ca77eb214e68b
Rajavenu Kyatham <quic_rkyatham@quicinc.com>
2024-11-13 18:14:37 +0530

    sdmclient: don't process pending config in case of            reconfigure comes as previous config.
    - use case: X config --> OFF --> Y (pending) --> ON --> again X config
    - ignore the Y config
    
    Change-Id: I22d1da01bd5eb967f06d8a00b89f5289b92314e1

--- sdmclient/sdm_display.cpp
+++ sdmclient/sdm_display.cpp
@@ -1363,6 +1363,7 @@ DisplayError SDMDisplay::SetActiveConfig(Config config) {
       DisplayError error = SetFBForExtendedResolution(config, &is_vconfig_fps_switched);
       if (error != kErrorNone || !is_vconfig_fps_switched) {
         pending_config_ = false;
+        pending_refresh_rate_config_ = UINT_MAX; /* Invalid config to skip */
         return error;
       }
     }
@@ -3033,6 +3034,7 @@ DisplayError SDMDisplay::SetActiveConfigWithConstraints(
           fb_height_ = info_client_requested.y_pixels;
         }
         pending_config_ = false;
+        pending_refresh_rate_config_ = UINT_MAX; /* Invalid config to skip */
         return error;
       }
     }
