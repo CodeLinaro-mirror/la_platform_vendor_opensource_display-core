af10c5d49cda94f8abda3c9d11969f02312767b0
Qing Huang <quic_huangq@quicinc.com>
2024-12-04 15:54:36 +0800

    sdm: disable demura for hand off
    If UEFI enabled the demura, after hand off to kernel, it needs
    to enable or disable demura feature on first commit. Else will
    see frame done timeout issue.
    Change adds to disable demura if demura property set to disable.
    
    Change-Id: I5e14c07782d108ce25fa26e5cfb27d33bbc82413

--- sdm/libs/core/display_builtin.cpp
+++ sdm/libs/core/display_builtin.cpp
@@ -24,7 +24,7 @@
 
 /*
 * Changes from Qualcomm Innovation Center, Inc. are provided under the following license:
-* Copyright (c) 2022-2024 Qualcomm Innovation Center, Inc. All rights reserved.
+* Copyright (c) 2022-2025 Qualcomm Innovation Center, Inc. All rights reserved.
 * SPDX-License-Identifier: BSD-3-Clause-Clear
 */
 
@@ -347,6 +347,7 @@ DisplayError DisplayBuiltIn::Init() {
     abc_prop_ = abc_tvm_enabled_;
 #endif
 
+    DisabelDemuraForHandOff();
     Debug::Get()->GetProperty(ENABLE_DEMURA, &demura_prop_);
     if (demura_prop_) {  // Create parser manager for demura
       pm_intf_ = pf_factory_->CreateDemuraParserManager(ipc_intf_, buffer_allocator_);
@@ -5173,6 +5174,24 @@ int DisplayBuiltIn::HandleTvmServiceEvent(const TvmServiceCbEvent &event) {
 int DisplayBuiltIn::Notify(const TvmServiceCbEvent &event) {
   std::thread([=] { DisplayBuiltIn::HandleTvmServiceEvent(event); }).detach();
   return 0;
+}
+
+DisplayError DisplayBuiltIn::DisabelDemuraForHandOff() {
+  if (!prop_intf_) {
+    DLOGE("prop_intf_ is nullptr");
+    return kErrorParameters;
+  }
+
+  PanelFeaturePropertyInfo payload = {};
+  payload.prop_id = kPanelFeatureDemuraInitCfg;
+
+  int ret = prop_intf_->SetPanelFeature(payload);
+  if (ret) {
+    DLOGE("Failed to SetPanelFeature, ret %d", ret);
+    return kErrorUndefined;
+  }
+
+  return kErrorNone;
 }
 
 }  // namespace sdm
