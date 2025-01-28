8c09efcf7150daf124f92fb1079b6c9ef3d4dd10
Pritama Biswas <quic_pritbisw@quicinc.com>
2025-01-28 16:56:46 +0530

    display-core: resolve compilation errors
    Change-Id: I29c9af13087333e96f18dd5ce205fe08bf242a90

--- sdmclient/sdm_layers.cpp
+++ sdmclient/sdm_layers.cpp
@@ -30,7 +30,7 @@
  * Changes from Qualcomm Innovation Center, Inc. are provided under the
  * following license:
  *
- * Copyright (c) 2022-2024 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2022-2025 Qualcomm Innovation Center, Inc. All rights reserved.
  * SPDX-License-Identifier: BSD-3-Clause-Clear
  */
 #include "sdm_layers.h"
@@ -117,7 +117,7 @@ SDMLayer::SDMLayer(Display display_id, BufferAllocator *buf_allocator)
     : SDMLayer(display_id, id_mgr_.GetNextPossibleId(), buf_allocator) {}
 
 SDMLayer::SDMLayer(Display display_id, LayerId layer_id, BufferAllocator *buf_allocator)
-    : display_id_(display_id), id_(id_mgr_.CreateId(layer_id)), buffer_allocator_(buf_allocator) {
+    : id_(id_mgr_.CreateId(layer_id)), display_id_(display_id), buffer_allocator_(buf_allocator) {
   layer_ = new Layer();
   geometry_changes_ |= kAdded;
 
