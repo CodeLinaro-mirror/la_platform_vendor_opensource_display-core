ffe1fbb3d8e7c7c18586ff25455de22f8caeefe1
Sabarinath M B <quic_sabamb@quicinc.com>
2025-03-14 20:01:06 +0530

    sdm: Move HdrEotf from hw_info_types to sdm_types
    Move HdrEotf to sdm_types to access enum values in composer.
    
    Change-Id: I8342095be7e78256bde955d405657d8d1b493d88

--- sdm/include/core/sdm_types.h
+++ sdm/include/core/sdm_types.h
@@ -32,7 +32,7 @@
 /*
 * Changes from Qualcomm Innovation Center are provided under the following license:
 *
-* Copyright (c) 2022-2024 Qualcomm Innovation Center, Inc. All rights reserved.
+* Copyright (c) 2022-2025 Qualcomm Innovation Center, Inc. All rights reserved.
 *
 * Redistribution and use in source and binary forms, with or without
 * modification, are permitted (subject to the limitations in the
@@ -392,6 +392,14 @@ enum SDMRenderIntent {
   ENHANCE = 1,
   TONE_MAP_COLORIMETRIC = 2,
   TONE_MAP_ENHANCE = 3
+};
+
+enum SDMHdrEotf {
+  kHdrEOTFInvalid = 0,
+  kHdrEOTFSDR = 0x1,
+  kHdrEOTFHdrLumRange = 0x2,
+  kHdrEOTFHDR10 = 0x4,
+  kHdrEOTFHLG = 0x8,
 };
 
 enum SDMPowerMode {
