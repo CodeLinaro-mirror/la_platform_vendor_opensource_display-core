a42d7046e6bddd9c35ada4d6c6c3ed3d500579a8
Anjaneya Prasad Musunuri <quic_aprasad@quicinc.com>
2025-04-24 16:54:34 +0530

    display-core: reset ucsc properties when plane is unstaged
    This change resets all ucsc properties when a plane is unstaged.
    
    Change-Id: I0ac48106efc75f65f0ffe2101fc1735a27a29258

--- sde-drm/drm_plane.cpp
+++ sde-drm/drm_plane.cpp
@@ -28,40 +28,10 @@
 */
 
 /*
-* Changes from Qualcomm Innovation Center are provided under the following license:
-*
-* Copyright (c) 2022-2025 Qualcomm Innovation Center, Inc. All rights reserved.
-*
-* Redistribution and use in source and binary forms, with or without
-* modification, are permitted (subject to the limitations in the
-* disclaimer below) provided that the following conditions are met:
-*
-* * Redistributions of source code must retain the above copyright
-* notice, this list of conditions and the following disclaimer.
-*
-* * Redistributions in binary form must reproduce the above
-* copyright notice, this list of conditions and the following
-* disclaimer in the documentation and/or other materials provided
-* with the distribution.
-*
-* * Neither the name of Qualcomm Innovation Center, Inc. nor the names of its
-* contributors may be used to endorse or promote products derived
-* from this software without specific prior written permission.
-*
-* NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
-* GRANTED BY THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
-* HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
-* WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-* MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-* IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
-* ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-* DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
-* GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-* INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
-* IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
-* OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
-* IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
+ * Changes from Qualcomm Technologies, Inc. are provided under the following license:
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ * SPDX-License-Identifier: BSD-3-Clause-Clear
+ */
 
 #include <stdint.h>
 #include <stdlib.h>
@@ -1940,6 +1910,13 @@ void DRMPlane::Unset(bool is_commit, drmModeAtomicReq *req) {
   PerformWrapper(DRMOps::PLANE_SET_FP16_UNMULT_CONFIG, req, 0);
   drm_msm_fp16_gc fp16_gc_config = {.flags = 0, .mode = FP16_GC_MODE_INVALID};
   PerformWrapper(DRMOps::PLANE_SET_FP16_GC_CONFIG, req, &fp16_gc_config);
+
+  // Reset ucsc properties
+  PerformWrapper(DRMOps::PLANE_SET_UCSC_UNMULT_CONFIG, req, false);
+  PerformWrapper(DRMOps::PLANE_SET_UCSC_IGC_CONFIG, req, DRMUcscIgcMode::UCSC_IGC_MODE_DISABLE);
+  PerformWrapper(DRMOps::PLANE_SET_UCSC_CSC_CONFIG, req, nullptr);
+  PerformWrapper(DRMOps::PLANE_SET_UCSC_GC_CONFIG, req, DRMUcscGcMode::UCSC_GC_MODE_DISABLE);
+  PerformWrapper(DRMOps::PLANE_SET_UCSC_ALPHA_DITHER_CONFIG, req, false);
 
   tmp_prop_val_map_.clear();
   committed_prop_val_map_.clear();
