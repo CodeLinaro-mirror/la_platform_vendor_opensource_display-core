084fd102639bfa41ffb310015178b167e5b7d1ca
Bharath Dasyam <bdasyam@qti.qualcomm.com>
2025-04-30 14:16:51 +0530

    sdm: refine thread scheduling based on compresmgr lib
    1. Use the value of compresmgr library to create thread if successfully, otherwise use the default value.
    2. Refine thread name to meet the limitation of linux that the length of the thread name cannot exceed 16 characters.
    
    Change-Id: I2c1119e7a74c854f34e039c8453bd348cf0588ca
    Signed-off-by: Bharath Dasyam <bdasyam@qti.qualcomm.com>

--- sdm/libs/dal/hw_events_drm.cpp
+++ sdm/libs/dal/hw_events_drm.cpp
@@ -29,7 +29,7 @@
 
 /*
 * Changes from Qualcomm Innovation Center are provided under the following license:
-* Copyright (c) 2022-2024 Qualcomm Innovation Center, Inc. All rights reserved.
+* Copyright (c) 2022-2025 Qualcomm Innovation Center, Inc. All rights reserved.
   SPDX-License-Identifier: BSD-3-Clause-Clear
 */
 
@@ -58,6 +58,16 @@
 #include <string>
 
 #include "hw_events_drm.h"
+
+#if defined(RT_SCHEDULE)
+#ifdef __cplusplus
+extern "C" {
+#endif
+#include "amss/compresmgr_client_api.h"
+#ifdef __cplusplus
+}
+#endif
+#endif
 
 #ifndef DRM_EVENT_MMRM_CB
 #define DRM_EVENT_MMRM_CB 0X8000000B
@@ -273,6 +283,15 @@ void HWEventsDRM::PopulateHWEventData(const vector<HWEvent> &event_list) {
 
 DisplayError HWEventsDRM::Init(DisplayId display_id, uint32_t core_id, SDMDisplayType display_type,
                                HWEventHandler *event_handler, const vector<HWEvent> &event_list) {
+#if defined(RT_SCHEDULE)
+  CPUConfigReq_t  cpuConfigReq  = {0};
+  CPUConfigResp_t cpuConfigResp = {0};
+  std::string     procName      = "display";
+  std::string     thread_name   = event_thread_name_;
+  CompResmgrRet_e ret;
+  struct sched_param params;
+#endif
+
   if (!event_handler)
     return kErrorParameters;
 
@@ -290,7 +309,7 @@ DisplayError HWEventsDRM::Init(DisplayId display_id, uint32_t core_id, SDMDispla
   poll_fds_.resize(event_list.size());
 
   DLOGI("poll_fd size %d", (int)poll_fds_.size());
-  event_thread_name_ += " - " + std::to_string(display_id.GetDisplayId()) + "-" +
+  event_thread_name_ += "-" + std::to_string(display_id.GetDisplayId()) + "-" +
                         std::to_string(display_type);
 
   PopulateHWEventData(event_list);
@@ -299,6 +318,26 @@ DisplayError HWEventsDRM::Init(DisplayId display_id, uint32_t core_id, SDMDispla
     DLOGE("Failed to start %s, error = %s", event_thread_name_.c_str(), strerror(errno));
     return kErrorResources;
   }
+
+#if defined(RT_SCHEDULE)
+  memcpy(cpuConfigReq.procName, procName.c_str(), procName.length() + 1);
+  memcpy(cpuConfigReq.thrdGrpName, thread_name.c_str(), thread_name.length() + 1);
+
+  ret = CompResmgrGetCPUConfig(&cpuConfigReq, &cpuConfigResp);
+
+  if (COMPRESMGR_RET_SUCCESS == ret) {
+    memset((char *)&params, 0x00, sizeof(struct sched_param));
+    params.sched_priority = cpuConfigResp.priority;
+
+    if (0 != pthread_setname_np(event_thread_, event_thread_name_.c_str())) {
+      DLOGE("pthread_setname_np: %s failed", event_thread_name_.c_str());
+    } else if (0 != pthread_setschedparam(event_thread_, cpuConfigResp.schedPolicy, &params)) {
+      DLOGE("pthread_setschedparam: %s failed", event_thread_name_.c_str());
+    }
+  } else {
+    DLOGE("CompResmgrGetCPUConfig: %s failed", event_thread_name_.c_str());
+  }
+#endif
 
   int value = 0;
   if (Debug::Get()->GetProperty(DISABLE_HW_RECOVERY_PROP, &value) == kErrorNone) {
