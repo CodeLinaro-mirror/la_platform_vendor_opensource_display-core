yoriscape/display

sdm: add support of C8 format.
- DRM_FORMAT_C8 is a FSC and compressed format.

CRs-Fixed: 3151068
Change-Id: Ie4a5e9d6a345b2ca0690d20f6d8dfe73b3e7f958

sdm/libs/utils/formats.cpp
@@ -52,6 +52,7 @@ bool IsUBWCFormat(LayerBufferFormat format) {
   case kFormatYCbCr420TP10Ubwc:
   case kFormatYCbCr420P010Ubwc:
   case kFormatRGBA16161616FUbwc:
+  case kFormatC8Ubwc:
     return true;
   default:
     return false;
@@ -143,6 +144,7 @@ const char *GetFormatString(const LayerBufferFormat &format) {
   case kFormatRGBA8888Ubwc:             return "RGBA_8888_UBWC";
   case kFormatRGBX8888Ubwc:             return "RGBX_8888_UBWC";
   case kFormatBGR565Ubwc:               return "BGR_565_UBWC";
+  case kFormatC8Ubwc:                   return "C8_UBWC_FSC";
   case kFormatYCbCr420Planar:           return "Y_CB_CR_420";
   case kFormatYCrCb420Planar:           return "Y_CR_CB_420";
   case kFormatYCrCb420PlanarStride16:   return "Y_CR_CB_420_STRIDE16";
@@ -251,6 +253,7 @@ float GetBufferFormatBpp(LayerBufferFormat format) {
     case kFormatYCbCr420SPVenusTile:
       return 1.5f;
     case kFormatA8:
+    case kFormatC8Ubwc:
       return 1.0f;
     default:
       return 0.0f;

CodeLinaro-mirror/la_platform_vendor_opensource_display-core

sdm/libs/utils/formats.cpp
@@ -55,6 +55,7 @@ bool IsUBWCFormat(LayerBufferFormat format) {
   case kFormatRGBA8888UbwcLossy2To1:
   case kFormatRGBA8888UbwcLossy8To5:
   case kFormatYCbCr422P210Ubwc:
+  case kFormatC8Ubwc:
     return true;
   default:
     return false;
@@ -150,6 +151,7 @@ const char *GetFormatString(const LayerBufferFormat &format) {
   case kFormatRGBA8888Ubwc:             return "RGBA_8888_UBWC";
   case kFormatRGBX8888Ubwc:             return "RGBX_8888_UBWC";
   case kFormatBGR565Ubwc:               return "BGR_565_UBWC";
+  case kFormatC8Ubwc:                   return "C8_UBWC";
   case kFormatYCbCr420Planar:           return "Y_CB_CR_420";
   case kFormatYCrCb420Planar:           return "Y_CR_CB_420";
   case kFormatYCrCb420PlanarStride16:   return "Y_CR_CB_420_STRIDE16";
@@ -266,6 +268,7 @@ float GetBufferFormatBpp(LayerBufferFormat format) {
     case kFormatYCbCr420SPVenusTile:
       return 1.5f;
     case kFormatA8:
+    case kFormatC8Ubwc:
       return 1.0f;
     default:
       return 0.0f;