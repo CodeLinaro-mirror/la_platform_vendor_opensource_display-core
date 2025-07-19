5632e08570bff6feb06254f22d33043cac86dcac
Mohit Sharma <smohit@qti.qualcomm.com>
2025-07-19 00:54:27 -0700

    Revert "display-core: Add macros for alor target"
    This reverts commit 9ec97a1486ae5e57eb6724241dec90db0fe31770.
    
    Change-Id: Ic7f3d68d28393a5fa1411e0d6323f0fd5525197e

--- sdm/libs/utils/utils.cpp
+++ sdm/libs/utils/utils.cpp
@@ -223,7 +223,6 @@ const char* GetSocName() {
     case kNiobeSocId2:
       return kNiobeSocName;
     case kCanoeSocId:
-    case kCanoeSocId1:
       return kCanoeSocName;
     default:
       return "";
