0760f68fe55638d7f5899d7a89768f1f89d7edd2
DHRUV KAUSHIK <dhrukaus@qti.qualcomm.com>
2025-07-21 15:21:57 +0530

    sdmclient: Handle single panel dead event
    Change-Id: Ib6ff336f171c2e7d08b92271ef6b4c8c9b326f45

--- sdmclient/concurrency_mgr.cpp
+++ sdmclient/concurrency_mgr.cpp
@@ -211,6 +211,11 @@ DisplayError ConcurrencyMgr::Init(BufferAllocator *buffer_allocator, SocketHandl
   DLOGI("disable_get_screen_decorator_support: %d",
         disable_get_screen_decorator_support_);
 
+  value = 0;
+  Debug::Get()->GetProperty(ENABLE_SELECTIVE_PANEL_DEAD, &value);
+  selective_panel_dead_ = (value == 1);
+  DLOGI("selective_panel_dead: %d", selective_panel_dead_);
+
   auto err = InitSubModules(debug);
   if (err != kErrorNone) {
     return err;
@@ -1323,7 +1328,7 @@ void ConcurrencyMgr::CompositorSync(CompositorSyncType sync_type) {
   }
 }
 
-void ConcurrencyMgr::PerformDisplayPowerReset() {
+void ConcurrencyMgr::PerformDisplayPowerReset(int32_t recovery_display) {
   disp_->RemoveDisconnectedPluggableDisplays();
 
   // Wait until all commands are flushed.
@@ -1342,6 +1347,9 @@ void ConcurrencyMgr::PerformDisplayPowerReset() {
   for (Display display = SDM_DISPLAY_PRIMARY; display < kNumDisplays;
        display++) {
     if (sdm_display_[display] != NULL) {
+      if (selective_panel_dead_ && (display != recovery_display)) {
+        continue;
+      }
       last_power_mode[display] = sdm_display_[display]->GetCurrentPowerMode();
       DLOGI("Powering off display = %d", INT32(display));
       status = sdm_display_[display]->SetPowerMode(SDMPowerMode::POWER_MODE_OFF,
@@ -1356,6 +1364,9 @@ void ConcurrencyMgr::PerformDisplayPowerReset() {
   for (Display display = SDM_DISPLAY_PRIMARY; display < kNumDisplays;
        display++) {
     if (sdm_display_[display] != NULL) {
+      if (selective_panel_dead_ && (display != recovery_display)) {
+        continue;
+      }
       SDMPowerMode mode = last_power_mode[display];
       DLOGI("Setting display %d to mode = %d", INT32(display), mode);
       status = sdm_display_[display]->SetPowerMode(mode, false /* teardown */);
@@ -1396,10 +1407,10 @@ void ConcurrencyMgr::PerformDisplayPowerReset() {
   }
 }
 
-void ConcurrencyMgr::DisplayPowerReset() {
+void ConcurrencyMgr::DisplayPowerReset(int32_t display) {
   // Do Power Reset in a different thread to avoid blocking of SDM event thread
   // when disconnecting display.
-  std::thread(&ConcurrencyMgr::PerformDisplayPowerReset, this).detach();
+  std::thread(&ConcurrencyMgr::PerformDisplayPowerReset, this, display).detach();
 }
 
 void ConcurrencyMgr::VmReleaseDone(Display display) {
