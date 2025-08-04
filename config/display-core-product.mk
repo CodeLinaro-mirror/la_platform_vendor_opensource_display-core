e0755bbad211cd1c8c2086d494d2085021371fce
Rheygine Medel <rmedel@qti.qualcomm.com>
2025-08-04 11:36:19 -0700

    sdm: Add PrivacyRegionManager
    This change introduces PrivacyRegionManager class, which is
    responsible for handling the privacy regions of the layers.
    
    Change-Id: I436f95904c850ade4f289886f766c6c460ac8136
    CRs-Fixed: 4223369

--- config/display-core-product.mk
+++ config/display-core-product.mk
@@ -67,3 +67,6 @@ ifneq ($(TARGET_HAS_LOW_RAM),true)
 #Multi-stc libraries config xml file
 PRODUCT_COPY_FILES += vendor/qcom/opensource/display-core/config/snapdragon_color_libs_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/snapdragon_color_libs_config.xml
 endif
+
+PRIVACY_REGIONS_OFFSETS_XML_PATH := vendor/qcom/opensource/display-core/config
+PRODUCT_COPY_FILES += $(PRIVACY_REGIONS_OFFSETS_XML_PATH)/privacy_regions_offsets.xml:$(TARGET_COPY_OUT_VENDOR)/etc/display/privacy_regions_offsets.xml
