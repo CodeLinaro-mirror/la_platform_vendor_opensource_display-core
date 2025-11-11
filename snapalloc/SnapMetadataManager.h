2976d5be460619ea6bc714c4616ec55509d5ab14
Tommy Sandanasamy <tsandana@qti.qualcomm.com>
2025-11-11 06:59:17 -0800

    snapalloc: Added ROI Metadata to SnapMetadata
    - Added a ROI Metadata helper function
    
    - Added ROI Metadata type into existing SnapMetadata
      class and modified existing methods
    
    Change-Id: I0a383d32d7ba2e6ff15b30a23eefe67509a560ac

--- snapalloc/SnapMetadataManager.h
+++ snapalloc/SnapMetadataManager.h
@@ -253,6 +253,8 @@ class SnapMetadataManager {
                                       BufferDescriptor *buf_des = nullptr);
   Error ViewIdHelper(SnapMetadata *metadata, SnapHandleInternal *handle, void *in_set = nullptr,
                      void *out_get = nullptr, BufferDescriptor *buf_des = nullptr);
+  Error ROIRectMetadataHelper(SnapMetadata *metadata, SnapHandleInternal *handle, void *in_set,
+                              void *out_get, BufferDescriptor *buf_des);
   Error CWBMetadataHelper(SnapMetadata *metadata, SnapHandleInternal *handle,
                           void *in_set = nullptr, void *out_get = nullptr,
                           BufferDescriptor *buf_des = nullptr);
@@ -493,6 +495,7 @@ class SnapMetadataManager {
           {MULTI_VIEW_INFO, &SnapMetadataManager::MultiViewHelper},
           {THREE_DIMENSIONAL_REF_INFO, &SnapMetadataManager::ThreeDimensionalRefInfoHelper},
           {VIEW_ID, &SnapMetadataManager::ViewIdHelper},
+          {ROI_RECT_METADATA, &SnapMetadataManager::ROIRectMetadataHelper},
           {CWB_METADATA, &SnapMetadataManager::CWBMetadataHelper},
   };
   struct metadata_traits {
@@ -566,6 +569,7 @@ class SnapMetadataManager {
           {MULTI_VIEW_INFO, {false}},
           {THREE_DIMENSIONAL_REF_INFO, {true}},
           {VIEW_ID, {true}},
+          {ROI_RECT_METADATA, {true}},
           {CWB_METADATA, {true}},
       };
 };
