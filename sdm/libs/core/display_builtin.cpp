3185a79ba36ae73b0aeab18a5e45c387d869d309
SHREYAS JADHAV <shrejadh@qti.qualcomm.com>
2025-11-27 13:14:25 +0530

    sdm: Enable idle pc event for VHM panel
    Change-Id: I250f0637ad2a9d1f5a13968693bc0e579c288ea6

--- sdm/libs/core/display_builtin.cpp
+++ sdm/libs/core/display_builtin.cpp
@@ -250,7 +250,7 @@ DisplayError DisplayBuiltIn::Init() {
             HWEvent::MMRM,
             HWEvent::VM_RELEASE_EVENT,
             HWEvent::VM_RECLAIM_EVENT};
-  if (client_ctx_.hw_panel_info.mode == kModeCommand) {
+  if ((client_ctx_.hw_panel_info.mode == kModeCommand) || client_ctx_.hw_panel_info.vhm_support) {
     events.push_back(HWEvent::IDLE_POWER_COLLAPSE);
   }
 #endif
