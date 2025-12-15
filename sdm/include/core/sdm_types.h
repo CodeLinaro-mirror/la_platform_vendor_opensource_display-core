c64316465ee9affe0a2002aba56f7a273d59255c
miche <miche@qti.qualcomm.com>
2025-12-15 19:11:40 +0800

    sdm: update copyright
    Remove outdated copyrights.
    
    Ticket: QUICLOST-821927
    
    Change-Id: If5dacdf5b199ff93c2294bd2bae19493c35b5b8a
    Signed-off-by: Min Cheng <miche@qti.qualcomm.com>

--- sdm/include/core/sdm_types.h
+++ sdm/include/core/sdm_types.h
@@ -1,8 +1,6 @@
 /*
 * Copyright (c) 2014 - 2019, 2021-2021, The Linux Foundation. All rights reserved.
 *
-* Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
-*
 * Redistribution and use in source and binary forms, with or without
 * modification, are permitted provided that the following conditions are
 * met:
