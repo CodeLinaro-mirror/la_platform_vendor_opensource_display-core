b170f1a622a9aa0aff41a778ee471272d631b267
Jagdish Prajapati <jprajapa@qti.qualcomm.com>
2026-01-21 02:22:10 +0530

    sdm-core/dal: Add APIs to expose WB capabilities
    - Add API to get WB Downscale capability.
    - Add API to get supported maximum WB block count.
    - Add API to get QRTC support capability.
    - Extend DRM connector driver capability loading
      implementation for WB downscale.
    
    Change-Id: I996b1a65495cd418ed155377f4755acdb8490a32

--- libdrmutils/drm_interface.h
+++ libdrmutils/drm_interface.h
@@ -1289,6 +1289,7 @@ struct DRMConnectorInfo {
   // Connection status of this connector
   bool is_connected;
   bool is_wb_ubwc_supported;
+  bool is_wb_downscale_supported = false;
   uint32_t topology_control;
   bool dyn_bitclk_support;
   std::vector<uint8_t> edid;
