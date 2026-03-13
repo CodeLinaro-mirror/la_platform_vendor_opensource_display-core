d7e9ea9580ec06b970def3971caa6fb23bc830ab
Pritam Samanta <pritsama@qti.qualcomm.com>
2026-03-13 10:11:20 +0530

    sdm: Fix format specifiers in privacy region debug logs
    Change-Id: I884ac152989afb82d838987fd62acc2997578bed

--- sdmclient/sdm_layers.cpp
+++ sdmclient/sdm_layers.cpp
@@ -1181,12 +1181,13 @@ DisplayError SDMLayer::SetLayerPrivacyRegions(const std::vector<PrivacyRegion> &
       if (Contains(dst_rect_, layer_rect) && (!is_area_mode || is_valid_index)) {
         layer_->privacy_regions.push_back(region);
       } else {
-        DLOGV_IF(
-            kTagClient,
-            "Invalid layer %d: region %f %f %f %f, dest_rect %f %f %f %f, privacy_region_mode %d "
-            "index %d",
-            id_, layer_rect.left, layer_rect.top, layer_rect.right, layer_rect.bottom,
-            dst_rect_.left, dst_rect_.top, dst_rect_.right, dst_rect_.bottom, mode, region.index);
+        DLOGV_IF(kTagClient,
+                 "Invalid layer %" PRIu64
+                 ": region %f %f %f %f, dest_rect %f %f %f %f, privacy_region_mode %d "
+                 "index %d",
+                 id_, layer_rect.left, layer_rect.top, layer_rect.right, layer_rect.bottom,
+                 dst_rect_.left, dst_rect_.top, dst_rect_.right, dst_rect_.bottom, mode,
+                 region.index);
       }
     }
   } else {
