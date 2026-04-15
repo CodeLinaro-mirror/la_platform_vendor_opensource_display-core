5d0d6c6c3a8ce9f417e056de44e08ff3c3fb1478
SHREYAS JADHAV <shrejadh@qti.qualcomm.com>
2026-04-15 19:38:16 +0530

    sdmclient:Enable readback buffer support on non-primary displays
    Remove the primary-only restriction for readback buffer attributes.
    This enables support for readback on other displays when available
    
    Change-Id: Ia1667ae0ce766a269499334397a89436bf084ba7

--- sdmclient/concurrency_mgr.cpp
+++ sdmclient/concurrency_mgr.cpp
@@ -1617,10 +1617,6 @@ DisplayError ConcurrencyMgr::GetReadbackBufferAttributes(Display display,
     return kErrorParameters;
   }
 
-  if (display != SDM_DISPLAY_PRIMARY) {
-    return kErrorNotSupported;
-  }
-
   SDMDisplay *sdm_display = sdm_display_[display];
   if (sdm_display == nullptr) {
     return kErrorParameters;
@@ -1647,10 +1643,6 @@ ConcurrencyMgr::SetReadbackBuffer(uint64_t display, void *buffer,
 
   if (display >= kNumDisplays) {
     return kErrorParameters;
-  }
-
-  if (display != SDM_DISPLAY_PRIMARY) {
-    return kErrorNotSupported;
   }
 
   CwbConfig cwb_config = {}; /* SF uses LM tappoint*/
@@ -1693,10 +1685,6 @@ ConcurrencyMgr::GetReadbackBufferFence(uint64_t display,
 
   if (display >= kNumDisplays) {
     return kErrorParameters;
-  }
-
-  if (display != SDM_DISPLAY_PRIMARY) {
-    return kErrorNotSupported;
   }
 
   return CallDisplayFunction(display, &SDMDisplay::GetReadbackBufferFence,
